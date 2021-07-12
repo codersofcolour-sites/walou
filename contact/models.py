88d282f8c55dfcc7399b82097f3d3f3e3df43d55
adedapoadejonwo <adejonwodapo@gmail.com>
2021-07-12 22:59:15 +0100

    migrations made for donate and contact page

--- contact/models.py
+++ contact/models.py
@@ -5,9 +5,11 @@ from wagtail.admin.edit_handlers import (
     FieldPanel,
     FieldRowPanel,
     InlinePanel,
-    MultiFieldPanel
+    MultiFieldPanel,
+    StreamFieldPanel
 )
-from wagtail.core.fields import RichTextField
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.contrib.forms.models import (
     AbstractEmailForm,
     AbstractFormField,
@@ -28,8 +30,13 @@ class ContactPage(AbstractEmailForm):
 
     intro = RichTextField(blank = True)
     thank_you_text = RichTextField(blank = True)
+    
+    contact_content = StreamField([
+        ('subheading', blocks.RichTextBlock()),        
+    ], null = True)
 
     content_panels = AbstractEmailForm.content_panels + [
+        StreamFieldPanel('contact_content'),      
         FieldPanel('intro'),
         InlinePanel('form_fields', label ='Form Fields'),
         FieldPanel('thank_you_text'),
